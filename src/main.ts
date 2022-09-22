5f70bdceff0cfce0b96c54dfc050e24e633b166d
kirill Iluschenko <kirill.i@nekidaem.ru>
2022-09-22 04:25:11 +0300

    added calendar, mock data, date redrawing logic

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import VCalendar from "v-calendar";
+import "v-calendar/dist/style.css";
 
-import './assets/main.css'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "./assets/main.css";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(VCalendar, {});
+
+app.mount("#app");
